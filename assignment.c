31a572208dfab31c8c6ffb4f5d8a6f07a6935d71
Lucas <Lucas@gelmac.de>
2021-11-30 13:44:41 +0100

    cleaned up

--- assignment.c
+++ assignment.c
@@ -4,29 +4,44 @@
 struct product{
   int importflag;
   int taxflag;
+  int amount;
   float price;
+  char name[20];
 } ;
-float taxsum(struct product);
+typedef struct product Product;
+
+float taxsum(struct product a);
+void printRecipe(struct product b);
 
 int main(){
   float sum = 0;
   float sumtax = 0;
-  struct product book = {0, 0, 12.49};
-  struct product musiccd = {0, 1, 14.99};
-  struct product chocobar = {0, 0, 0.85};
+  struct product product1 = {0, 0, 1, 12.49,"book"};
+  struct product product2 = {0, 1, 1, 14.99, "music CD"};
+  struct product product3 = {0, 0, 1, 0.85, "chocolate bar"};
 
-  sumtax += taxsum(book);
-  sumtax += taxsum(musiccd);
-  sumtax += taxsum(chocobar);
-  sum = book.price + musiccd.price + chocobar.price;
 
-  printf("Sales Taxes: %f\n", sumtax);
-  printf("Total: %f\n", sum );
+  sumtax += taxsum(product1);
+  sumtax += taxsum(product2);
+  sumtax += taxsum(product3);
+  sum = product1.price + product2.price + product3.price + sumtax;
+
+  printRecipe(product1);
+  printRecipe(product2);
+  printRecipe(product3);
+
+  printf("Sales Taxes: %.2f\n", sumtax);
+  printf("Total: %.2f\n", sum );
 }
 
 float taxsum(struct product a){
-  float sum;
-  If(a.taxflag == 1) sum += a.price*.1;
-  If(a.importflag == 1) sum += a.price*.05;
-  return sum;
+  if (a.taxflag == 1) {
+    return a.price*0.1;
+  }
+    return 0;
+}
+
+void printRecipe(struct product a){
+  printf("%d %s\t", a.amount, a.name);
+  printf("%.2f\n", a.price + taxsum(a));
 }
